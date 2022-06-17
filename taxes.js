a30724f3fc2e7f97cae2b5cfcbea91527d86c599
Dom26 <domniniquemoore12@yahoo.com>
2022-06-17 16:20:31 -0700

    taxes.js: can get the user inputted data and display it back to the user

--- taxes.js
+++ taxes.js
@@ -1,3 +1,14 @@
 const taxRates = [10,12,22,24,32,35,37]
 const maxIncomes = [10275,41775,89075,170050,215950,539900]
 
+let userInput = document.getElementById("userInput").value
+let button = document.getElementById("submitBtn")
+
+// Add event listener to button
+button.addEventListener('click',getData)
+
+// Function to get the data the user entered in the input box
+function getData(){
+    userInput = document.getElementById("userInput").value
+    alert(userInput)
+}
