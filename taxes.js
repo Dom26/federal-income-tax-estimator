7ac52d4149338db6bebcda5414c367f47aee27bc
Dom26 <domniniquemoore12@yahoo.com>
2022-06-23 01:28:09 -0700

    main.html: added an id for main tag/ taxes.js: able to calculate and display data to html

--- taxes.js
+++ taxes.js
@@ -3,6 +3,7 @@ const taxRates = [0.10,0.12,0.22,0.24,0.32,0.35,0.37]
 // The maximum incomes at each Income bracket
 const maxIncomes = [10275,41775,89075,170050,215950,539900]
 
+let mainElement = document.getElementById("main")
 //let userInput = document.getElementById("userInput").value
 let button = document.getElementById("submitBtn")
 
@@ -36,12 +37,54 @@ function calculateTaxes(annualIncome,maxIncomeArr){
     }
     return totalTaxes
 }
+
+// Function to create the div that displays the data
+function createDataDiv(){
+    // Remove the div if it exists and create a new one
+    if(document.getElementById("data")){
+        document.getElementById("data").remove()
+    }
+    let dataDiv = document.createElement("div")
+    dataDiv.id = "data"
+    mainElement.appendChild(dataDiv)
+}
+
+// Function to display the data to the HTML dom
+function displayDataToHTML(grossIncome,hourlyRate,taxOwed){
+    let displayDiv = document.getElementById("data")
+
+    let pTagGross = document.createElement("p")
+    displayDiv.appendChild(pTagGross)
+    pTagGross.innerHTML = `Gross Annual Income: $${grossIncome.toFixed(2)}`
+    
+    let pTagHourly = document.createElement("p")
+    displayDiv.appendChild(pTagHourly)
+    pTagHourly.innerHTML = `Hourly Rate: $${hourlyRate.toFixed(2)}`
+    
+    let pTagTax = document.createElement("p")
+    displayDiv.appendChild(pTagTax)
+    pTagTax.innerHTML = `Taxes owed: $${taxOwed.toFixed(2)}`
+    
+    let pTagNet = document.createElement("p")
+    displayDiv.appendChild(pTagNet)
+    pTagNet.innerHTML = `Net Annual Income: $${(grossIncome - taxOwed).toFixed(2)}`
+    
+    let pTagNetMonthly = document.createElement("p")
+    displayDiv.appendChild(pTagNetMonthly)
+    pTagNetMonthly.innerHTML = `Net Monthly Income: $${((grossIncome - taxOwed) / 12).toFixed(2)}`
+}
+
 // Function to get the data the user entered in the input box
 function getData(){
     let userInput = document.getElementById("userInput").value
     
     let userIncome = Number(userInput)
+    let hourlyRate = userIncome / (12 * 4 * 40)
     let taxesOwed = calculateTaxes(userIncome,maxIncomes)
+    createDataDiv()
+    displayDataToHTML(userIncome,hourlyRate,taxesOwed)
+    //let netMonthlyIncome = (userIncome-taxesOwed) / 12
     
-    alert(`Gross Income: ${userIncome}\nTaxes owed: ${taxesOwed}\nNet Income: ${userIncome-taxesOwed}`)
+    
+    //alert(`Gross Income: ${userIncome.toFixed(2)}\nHourly Rate: ${hourlyRate.toFixed(2)}\nTaxes owed: ${taxesOwed.toFixed(2)}\nNet Income: ${(userIncome-taxesOwed).toFixed(2)}\nNet Monthly Income: ${netMonthlyIncome.toFixed(2)}`)
 }
